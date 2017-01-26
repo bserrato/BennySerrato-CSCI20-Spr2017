1d59d64c648a08decb0f047b20bf6f6b73bf153e
Benny Serrato <bserrato002@student.butte.edu>
2017-01-26 22:40:45 +0000

    lab1.1

---
+++ lab1.1/lab1.1.cpp
@@ -0,0 +1,7 @@
+/*
+The player will begin the guessing game and given instuctions on how the game will work
+The player must pick a number between 1 and 10
+If player chooses incorrectly the program will announce that the number will be "higher" or "lower" than the number thhat the player selected incorrectly
+The player will lose 1 point from 10 for every inncorrect
+If player chooses the correct number, they win and will be given their score out of ten
+/*
