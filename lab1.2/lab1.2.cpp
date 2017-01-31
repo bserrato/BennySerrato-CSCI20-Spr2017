3c6a3116d8fa2c4e2d1c2ed564b2fad8c1b5e4cb
Benny Serrato <bserrato002@student.butte.edu>
2017-01-31 23:34:53 +0000

    Lab1.2

---
+++ lab1.2/lab1.2.cpp
@@ -0,0 +1,36 @@
+//Created By: Benny Serrato
+//Created On: 1/31/17
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << endl;
+    cout << "                                                        eTRL" << endl;
+    cout << "                                                       M!!!9" << endl;
+    cout << "                                                        #@@" << endl;
+    cout << "                                                         RX)" << endl;
+    cout << "                                                         RNR" << endl;
+    cout << "                                                         RN$" << endl;
+    cout << "                                                         RNM" << endl;
+    cout << "                                                         B8WL" << endl;
+    cout << "                                                        @XUUUN..." << endl;
+    cout << "                                                    Z@TMMMNMMMMNMMMMRm_" << endl;
+    cout << "                                                uRNMNMNMNMNMNMNMNMNMNMMRc" << endl;
+    cout << "                                              =5MMNMNMNMNMNMNMNMNMNMNMNMM$_" << endl;
+    cout << "                                             dMNMNMNMNMNMNMNMNMNMNMNMNMNMNMr" << endl;
+    cout << "                                            JMMNMNMNMNMMNMNMNMNMNMNMNMNMNMNM_" << endl;
+    cout << "                                            MNMNMNMNMNMNMNMNMNMNMNMNMNMNMNMN$" << endl;
+    cout << "                                            MNMNMNMNMNMNMNMNMNMNMNMNMNMNMNMNM)" << endl;
+    cout << "                                            MNMNMNMNMNMNMNMNMNMNMNMNMNMNMNMNMR" << endl;
+    cout << "                                            MNMNMNMNMNMNMNMNMNMNMNMNMNMNMNMNM$...." << endl;
+    cout << "                                            MMMMMMM8*TTTTTTT???!!!!!!!!!!!!!!!!!!!!?N." << endl;
+    cout << "                                            &MMMM0?!!U*$$$$$$$$$$$$$$$$$$$$$**$$$$$N!!N" <<endl;
+    cout << "                                            4XMN$!!XF~~~~~~~~#*$$$$$$R#~~~~~~~~$$$$$$!!)" <<endl;
+    cout << "                                            4XMNE!!$~~~~~~~~~~~~~~?~~~~~~~(===!?$$$$$R!)" <<endl;
+    cout << "                                            *MMMN!!!&~~~~~~4$$E~~~X~~~~~~~~3$$E~4$$$$$E!)" <<endl;
+    cout << "                                             EM9!!!M=~~~~~~''''!~~~6~~~~~~~~~~($$$$$$!9" <<endl;
+    cout << "                                             RM@!!!!?i=~~~~~~~~~~($$N~~~~~~~~~\$$$$$#!XF" <<endl;
+    cout << "                                             $MNB!!!!!!!!TT#***@******##TTTTTT?!!!!!U#" << endl;
+}
