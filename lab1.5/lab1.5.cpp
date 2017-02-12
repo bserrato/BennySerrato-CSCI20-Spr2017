cfd4107145878242e8411ba25d23b50ab9315106
Benny Serrato <bserrato002@student.butte.edu>
2017-02-12 02:38:09 +0000

    Lab1.5 Resubmitted

--- lab1.5/lab1.5.cpp
+++ lab1.5/lab1.5.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 int main (){
 
-string firstNoun; //string created for the first noun in the Mad Lib
-string firstVerb; //string created for the first verb in the Mad Lib
-string firstAdjective; //string created for the first adjective in the Mad Lib
-string secondNoun; //string created for the first noun in the Mad Lib
-string secondVerb; //string created for the second verb in the Mad Lib
-string thirdNoun; //string created for the second verb in the Mad Lib
-string secondAdjective; //string created for the second adjective in the Mad Lib 
-string fourthNoun; //string created for the fourth Noun in the Mad Lib
+string firstNoun = "pizza"; //string created for the first noun in the Mad Lib
+string firstVerb = "cherish";//string created for the first verb in the Mad Lib
+string firstAdjective = "happy"; //string created for the first adjective in the Mad Lib
+string secondNoun = "salesman"; //string created for the first noun in the Mad Lib
+string secondVerb = "bring"; //string created for the second verb in the Mad Lib
+string thirdNoun = "house"; //string created for the second verb in the Mad Lib
+string fourthNoun = "floor"; //string created for the fourth Noun in the Mad Lib
+string secondAdjective = "lazy"; //string created for the second adjective in the Mad Lib 
 
 cout << "Hello welcome Mad libs we will give you input boxes to input while also telling what to input in the box such as when asking a noun input a noun and etc." << endl;
 /*Minor instruction so the user understands the program */
@@ -45,11 +45,12 @@ cin >> fourthNoun; //User inputs a noun
 
 cout << "Enter your second Adjective:";//User inputs a second adjective
 cin >> secondAdjective;//User inputs an adjective
+
 cout << endl;
 cout << "There once was a guy named Doug, who was looking for " << firstNoun << " around town." << endl;//First sentence of the Mad Lib
 cout << "Once Doug found "<< firstNoun <<", he " << firstVerb << "ed it or them." << endl;//Second sentence of the Mad Lib
 cout << "Afterwards, Doug found a " << firstAdjective << " " << secondNoun << " at a park bench.";//Thrid sentence of the Mad Lib
 cout << "Doug decided to " << secondVerb << " the " << firstAdjective << " " << secondNoun << " from the park bench to his " << thirdNoun << "." << endl;//Fourth sentence of the Mad Lib
-cout << "While Doug was taking " << firstAdjective << " " << secondNoun << ", Doug took a whif of " << thirdNoun << " and passed out on the " << fourthNoun << endl;//Fith sentence of the Mad Lib
+cout << "While Doug was taking " << firstAdjective << " " << secondNoun << ", Doug took a whif of his " << thirdNoun << " and passed out on the " << fourthNoun << " " << endl;//Fith sentence of the Mad Lib
 cout << "Thus concludes the adventures of " << secondAdjective << " Doug.";// Sixth and final sentence of the Mad Lib
 }
