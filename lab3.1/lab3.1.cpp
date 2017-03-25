f3f8dd071cfc9c4544bbc83268f22ae073d7eea6
Benny Serrato <bserrato002@student.butte.edu>
2017-03-25 06:37:42 +0000

    Lab3.1 Finished

--- lab3.1/lab3.1.cpp
+++ lab3.1/lab3.1.cpp
@@ -10,37 +10,204 @@ int main(){
     int dumbPhone;//amount of dumb phones 
     int smartPhone;//amount of smart phones
     int tabletPhone;//amount of tablets
-    int planPrice;//Price of the plan for phone 
+    double planPrice;//Price of the plan for phone 
     int dataPlan;//plan of amount of data
     int familyPlan;//amount of family members in plan
     string memberPlan;//check if a coporate member
     
     cout << "Dumb Phones: ";
-    cin >> dumbPhone;
+    cin >> dumbPhone;//Customer input for amount of dumb phones
     
     cout << "Smart Phones: ";
-    cin >> smartPhone;
+    cin >> smartPhone;//Customer input for amount of smart phones
     
     cout << "Tablets: ";
-    cin >> tabletPhone;
+    cin >> tabletPhone;//Customer input for amount of tablets
     
-    cout << "Family Members in Plan";
-    cin >> familyPlan;
+    cout << "Amount of Gigabytes: ";
+    cin >> dataPlan;//Customer input for amount of gigabytes in plan
     
-    cout << "Coporate Member?";
-    cin >> memberPlan;
-    if(tabletPhone > 0 && smartPhone > 0 && dumbPhone > 0 && dataPlan <= 5 && familyPlan >2 && memberPlan == "yes" || memberPlan == "Yes" || memberPlan == "Y"){
-        planPrice = (tabletPhone * 10)+(smartPhone * 5)+(dumbPhone * 0) + 30 + (familyPlan * 3) + 5;
+    cout << "Family Members in Plan: ";
+    cin >> familyPlan;//Customer input for amount of family members on plan
+    
+    cout << "Coporate Member?: ";
+    cin >> memberPlan;//Customer input for if the customer is a coperate member
+    
+    if(memberPlan == "yes" || memberPlan == "Yes" || memberPlan == "Y" || memberPlan == "YES" || memberPlan == "y"){//if the customer is a coportate member for the discount
+        if (dataPlan > 32){//cheaper to have unlimited data after 32 Gigabytes
+            planPrice = 75 + ((dataPlan - 10) * 1.50 );
+        
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+        else if(dataPlan <= 32 && dataPlan > 15){//Charge for data before 32 gigabytes
+            planPrice = 5 + ((dataPlan - 1) * .75 ) + ((dataPlan - 5) * 1.5) + ((dataPlan - 15) * 3);
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+        else if (dataPlan <= 15 && dataPlan > 5){//if customer wants more than 5 gigabytes and atleast 15 gigabytes
+        planPrice = 5 + ((dataPlan - 1) * .75 ) + ((dataPlan - 5) * 1.5);
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+        else{//if customer wants atleast 5 gigabytes
+            planPrice = 5 + ((dataPlan - 1) *.75);
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
         planPrice = planPrice - (planPrice * .10);
-    }
-    else if(tabletPhone > 0 && smartPhone > 0 && dumbPhone > 0 && dataPlan <= 5 && familyPlan > 2 && memberPlan == "no" || memberPlan == "No" || memberPlan == "N"){
-        planPrice = dumbPhone * 0;
+    }    
+    else{//if the customer isn't a coportate member for the discount
+       if (dataPlan > 32){//cheaper to have unlimited data after 32 Gigabytes
+            planPrice = 75 + ((dataPlan - 10) * 1.50 );
         
-    }
-    else if(tabletPhone > 0 && smartPhone > 0 && dumbPhone > 0 && dataPlan <= 5 && familyPlan >2 && memberPlan == "yes" || memberPlan == "Yes" || memberPlan == "Y"){
-        
-    }
-    
-    cout << "Charge: "<< planPrice << endl;
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+        else if(dataPlan <= 32 && dataPlan > 15){//Charge for data before 32 gigabytes
+            planPrice = 5 + ((dataPlan - 1) * .75 ) + ((dataPlan - 5) * 1.5) + ((dataPlan - 15) * 3);
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+        else if (dataPlan <= 15 && dataPlan > 5){//if customer wants more than 5 gigabytes and atleast 15 gigabytes
+        planPrice = 5 + ((dataPlan - 1) * .75 ) + ((dataPlan - 5) * 1.5);
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+        else{//if customer wants atleast 5 gigabytes
+            planPrice = 5 + ((dataPlan - 1) *.75);
+            if(tabletPhone >= 0 && smartPhone >= 0 && dumbPhone >= 0){//amount of phones the customer wants on the plan
+            planPrice = planPrice + (tabletPhone * 10) + (smartPhone * 5) + (dumbPhone * 0);
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+            else{//if customer does not want phones in plan
+            planPrice = planPrice;
+                if (familyPlan > 2){//if customer has more than 2 people in plan
+                    planPrice = planPrice + ((familyPlan - 2) * 3);
+                }
+                else{//if customers has 2 or less people in plan
+                    planPrice = planPrice;
+                }
+            }
+        }
+    }    
+    cout << "Total Charge: $" << planPrice << endl;
     return 0;
 }
