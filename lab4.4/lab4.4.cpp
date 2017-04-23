a97896f4236b0841e2fa000a8be6fdad6e414913
Benny Serrato <bserrato002@student.butte.edu>
2017-04-23 19:50:53 +0000

    Lab4.5

--- lab4.4/lab4.4.cpp
+++ lab4.4/lab4.4.cpp
@@ -12,7 +12,6 @@ int main(){
     const int LAST_NAME = 20;//size of the cstring for last name
     char firstName[FIRST_NAME];//cstring for the first name
     char lastName[LAST_NAME];//cstring for the last name
-    char storeName[2];//will store the first two letters of the first name
     int stringLength = 0;//will store the amount of characters in the first and last names
     cout << "Please enter your name in the order said." << endl;
     
@@ -52,7 +51,7 @@ int main(){
         cout << "Last Name: ";
         cin >> lastName;
         stringLength = strlen(lastName);
-        while(stringLength > FIRST_NAME){//check if the name fits inside the c string
+        while(stringLength > LAST_NAME){//check if the name fits inside the c string
             cout << "Your last name is too big!" << endl; 
             cout << "Please type in a shorter name that contains atleast 20 letters."<< endl;
             cout << "Last Name:";
@@ -66,19 +65,16 @@ int main(){
     for(int i = 0; i < LAST_NAME; i++){//Converts any uppercase in the last name to lowercase for usernames
             lastName[i] = tolower(lastName[i]);
     }
-    for(int i = 0; i < 2; i++){//stores the first two letters of the first name for user name purposes
-        storeName[i] = firstName[i];
-    }
     
     cout << "Name: " << firstName << " " << lastName << endl;
     cout << endl;
     cout << "Here are three names to choose from:" << endl;
     /* first of the user names*/
-    cout << storeName << lastName << endl;
+    cout << firstName[0] << firstName[1] << lastName << endl;
     /* second of the user names*/
     cout << firstName << lastName << endl;
     /*Third of the user names */
-    cout << storeName[0] <<  lastName << endl;
+    cout << firstName[0] <<  lastName << endl;
     
     return 0;
 }
---
+++ lab4.5/lab4.5.cpp
@@ -0,0 +1,68 @@
+//Created By: Benny Serrato
+//Created On: 4/23/2017
+//This program will create usernames using the user's name using strings
+
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+int main(){
+    string firstName;
+    string lastName;
+    
+    cout << "Please enter your name in the order said." << endl;
+    cout << "First Name:";
+    cin >> firstName;
+     while(firstName.length() > 10){
+        cout << "Your first name is too big!" << endl; 
+        cout << "Please type in a shorter name that contains atleast 10 letters."<< endl;
+        cout << "First Name:";
+        cin >> firstName;//prompts user to input their first name again 
+    }
+    cout << "Last Name: ";
+    cin >> lastName;
+    while(lastName.length() > 20){
+        cout << "Your last name is too big!" << endl; 
+        cout << "Please type in a shorter name that contains atleast 20 letters."<< endl;
+        cout << "Last Name:";
+        cin >> lastName;//prompts user to input their last name again
+    }
+    while(firstName == lastName){
+        cout << "Sorry you cannot have the same name for your first and last name." << endl;
+        cout << "First Name: ";
+        cin >> firstName;//prompts user to input their first name again 
+        while(firstName.length() > 10){
+            cout << "Your first name is too big!" << endl; 
+            cout << "Please type in a shorter name that contains atleast 10 letters."<< endl;
+            cout << "First Name:";
+            cin >> firstName;//prompts user to input their first name again 
+        }
+        cout << "Last Name: ";
+        cin >> lastName;//prompts user to input their last name again
+        while(lastName.length() > 20){
+            cout << "Your last name is too big!" << endl; 
+            cout << "Please type in a shorter name that contains atleast 20 letters."<< endl;
+            cout << "Last Name:";
+            cin >> lastName;//prompts user to input their last name again
+        }
+    }
+     for(int i = 0; i < firstName.length() - 1; i++){//Converts any upper case in the first name to lowercase for usernames
+          firstName.at(i) = tolower(firstName.at(i));
+    }
+    for(int i = 0; i < lastName.length() - 1; i++){//Converts any uppercase in the last name to lowercase for usernames
+            lastName.at(i) = tolower(lastName.at(i));
+    }
+    cout << "Name: " << firstName << " " << lastName << endl;
+    cout << endl;
+    cout << "Here are three names to choose from:" << endl;
+    
+    /*first of the user names*/
+    cout << firstName.substr(0,2) <<lastName << endl;
+    /*second of the user names*/
+    cout << firstName << lastName << endl;
+    /*third of the user names*/
+    cout << firstName.at(0) <<lastName << endl;
+    return 0;
+}
+    
